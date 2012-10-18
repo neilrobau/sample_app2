4cf7dc7ea60bf6162aba15f85bfc03a891f54aa8
neilrobau <neilrobau@gmail.com>
2012-10-19 00:19:48 +1100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp2::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   #match 'static_pages/home',    to: 'static_pages#home'  
   
  #get "static_pages/help"
   match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
